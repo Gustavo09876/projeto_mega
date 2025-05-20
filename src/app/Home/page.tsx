a9915ec12d06a189068d5af2bceb8d50b647df33
Gustavo09876 <gustavovinicius1379@gmail.com>
2025-05-19 22:19:27 -0400

    Uptade SideBar

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -159,6 +159,7 @@ export default function Home() {
           </div>
           {/* Botão de "Sair" com ícone */}
           <div style={{ display: 'flex', alignItems: 'center' }}>
+            <a href="/Login" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out-icon lucide-log-out"
             style={{ cursor: 'pointer' }}>
             <path d="m16 17 5-5-5-5"/><path d="M21 12H9"/><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/></svg>
@@ -167,6 +168,7 @@ export default function Home() {
               <SideBarTitle Text="Sair" />
             </div>
             )}
+            </a>
           </div>
         </div> 
       </div>
